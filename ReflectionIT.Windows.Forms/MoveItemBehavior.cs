7699532c02f4d6e251bf872bacabaafefc31fbfe
sonnemaf <fons.sonnemans@reflectionit.nl>
2021-12-13 12:55:34 +0100

    WaitCursor added

--- ReflectionIT.Windows.Forms/MoveItemBehavior.cs
+++ ReflectionIT.Windows.Forms/MoveItemBehavior.cs
@@ -9,47 +9,14 @@ namespace ReflectionIT.Windows.Forms {
     /// </summary>
     public class MoveItemBehavior : Component {
 
-		private Button _actionButton;
-		private ListBox _source;
-        private bool _doubleClickSupport = true;
+        private Button _actionButton;
+        private ListBox _source;
+        private IButtonControl _acceptButton;
 
-		private IButtonControl _acceptButton;
+        [DefaultValue(true)]
+        public bool DoubleClickSupport { get; set; } = true;
 
-		[DefaultValue(true)]
-		public bool DoubleClickSupport {
-            get => _doubleClickSupport;
-            set {
-                if (_doubleClickSupport && Source != null) {
-                    Source.DoubleClick -= new EventHandler(Source_DoubleClick);
-                    Source.Enter -= new EventHandler(Source_Enter);
-                    Source.Leave -= new EventHandler(Source_Leave);
-                }
-
-                _doubleClickSupport = value;
-
-                if (value && Source != null) {
-                    Source.DoubleClick += new EventHandler(Source_DoubleClick);
-                    Source.Enter += new EventHandler(Source_Enter);
-                    Source.Leave += new EventHandler(Source_Leave);
-                }
-            }
-        }
-
-        private void Source_DoubleClick(object sender, EventArgs e) {
-			ListBoxHelper.MoveItem(Source, Destination);
-		}
-
-		private void Source_Enter(object sender, EventArgs e) {
-			Form currentForm = Source.FindForm();
-			_acceptButton = currentForm.AcceptButton;
-			currentForm.AcceptButton = ActionButton;
-		}
-
-		private void Source_Leave(object sender, EventArgs e) {
-			Source.FindForm().AcceptButton = _acceptButton;
-		}
-
-		public Button ActionButton {
+        public Button ActionButton {
             get => _actionButton;
             set {
                 if (_actionButton is not null) {
@@ -65,16 +32,45 @@ namespace ReflectionIT.Windows.Forms {
         public ListBox Source {
             get => _source;
             set {
+                if (Source is not null) {
+                    Source.DoubleClick -= new EventHandler(Source_DoubleClick);
+                    Source.Enter -= new EventHandler(Source_Enter);
+                    Source.Leave -= new EventHandler(Source_Leave);
+                }
                 _source = value;
-                DoubleClickSupport = DoubleClickSupport;
+                if (Source is not null) {
+                    Source.DoubleClick += new EventHandler(Source_DoubleClick);
+                    Source.Enter += new EventHandler(Source_Enter);
+                    Source.Leave += new EventHandler(Source_Leave);
+                }
             }
         }
 
         public ListBox Destination { get; set; }
 
         private void ActionButton_Click(object sender, EventArgs e) {
-			ListBoxHelper.MoveItem(Source, Destination);
-		}
-	}
+            ListBoxHelper.MoveItem(Source, Destination);
+        }
+
+        private void Source_DoubleClick(object sender, EventArgs e) {
+            if (DoubleClickSupport) {
+                ListBoxHelper.MoveItem(Source, Destination);
+            }
+        }
+
+        private void Source_Enter(object sender, EventArgs e) {
+            if (DoubleClickSupport) {
+                Form currentForm = Source.FindForm();
+                _acceptButton = currentForm.AcceptButton;
+                currentForm.AcceptButton = ActionButton;
+            }
+        }
+
+        private void Source_Leave(object sender, EventArgs e) {
+            if (DoubleClickSupport) {
+                Source.FindForm().AcceptButton = _acceptButton;
+            }
+        }
+    }
 
 }
---
+++ ReflectionIT.Windows.Forms/WaitCursor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ReflectionIT.Windows.Forms {
+
+    /// <summary>
+    /// Make the current Cursor a WaitCursor and restore it on Dispose
+    /// <code>
+    /// using (new WaitCursor()) {
+    ///     // long running operation, for example:
+    ///     System.Threading.Thread.Sleep(1000);
+    /// }
+    /// </code>
+    /// </summary>
+    public sealed class WaitCursor : IDisposable {
+
+        private readonly Cursor _prev;
+
+
+        /// <summary>
+        /// Save the current cursor and then set it to WaitCursor
+        /// </summary>
+        public WaitCursor() {
+            _prev = Cursor.Current;
+            Cursor.Current = Cursors.WaitCursor;
+        }
+
+        /// <summary>
+        /// Restore the previous cursor
+        /// </summary>
+        public void Dispose() {
+            Cursor.Current = _prev;
+        }
+    }
+
+}
